734125a12014bbba7de5e3235f5525deae46bd0a
dwlvrr <128193069+dwlvrr@users.noreply.github.com>
2026-08-26 23:37:08 -0400

    npm start

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require('path')
 const axios = require('./speed-limiter')
 const cheerio = require('cheerio')
 
-const bookID = '9781319081799'
+const bookID = '9798214153520'
 const baseURL = `https://jigsaw.vitalsource.com/books/${bookID}/epub/`
 const fsRelativePath = './epub/'
 
 // Cookie value sent to jigsaw.vitalsource.com from the browser
 // including: reese84 + jigsaw_session
-const globalCookieVal = ``
+const globalCookieVal = `JSESSIONID=c246add651fdbd70`
 
 const writeIntoFS = async (filepath, contents) => {
   const file = path.resolve(fsRelativePath, filepath)
@@ -95,3 +95,4 @@ const recursiveGet = async (arrayURLs) => {
 }
 
 getContainer().then(getOPF).then(recursiveGet)
+
